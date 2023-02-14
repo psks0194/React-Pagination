de1880548537476648e87d46678621751e0845c1
Prashant Sing <psks0194@gmail.com>
2023-02-14 14:54:58 +0530

    react pagination implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [TotalProd, setTotalProd] = useState(0);
+
+  const fetchProducts = async () => {
+    const res = await fetch(
+      `https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`
+    );
+    const data = await res.json();
+    setTotalProd(data.total);
+    console.log(data);
+    setProducts(data.products);
+  };
+
+  const selectPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= TotalProd / 10 &&
+      selectedPage !== page
+    )
+      setPage(selectedPage);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, [page]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      {TotalProd > 0 && (
+        <div className="products">
+          {products.map((prod) => {
+            return (
+              <span className="product__single" key={prod.id}>
+                <img src={prod.thumbnail} alt={prod.title} />
+                <span>{prod.title}</span>
+              </span>
+            );
+          })}
+        </div>
+      )}
+      {TotalProd > 0 && (
+        <div className="pagination">
+          <span
+            className={page !== 1 ? "" : "hideArrow"}
+            onClick={() => selectPageHandler(page - 1)}
+          >
+            👈
+          </span>
+          {[...Array(TotalProd / 10)].map((_, i) => {
+            return (
+              <span
+                className={page === i + 1 ? "page_selected" : ""}
+                onClick={() => selectPageHandler(i + 1)}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+          <span
+            className={page !== TotalProd / 10 ? "" : "hideArrow"}
+            onClick={() => selectPageHandler(page + 1)}
+          >
+            👉
+          </span>
+        </div>
+      )}
     </div>
   );
 }
